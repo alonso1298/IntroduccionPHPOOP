920c9ef19c56e1e393e727b183e7c80d2c84e5cb
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-09-22 22:18:04 -0600

    Creando una clase e instanciandola

--- 01.php
+++ 01.php
@@ -1,4 +1,29 @@
 <?php include 'includes/header.php';
 
+// Definir o crear una clase
+class Producto {
+    public $nombre;
+    public $precio;
+    public $disponible;
+}
+
+$producto = new Producto();
+$producto->nombre = 'Tablet';
+$producto->precio = 200;
+$producto->disponible = true;
+
+echo '<pre>';
+var_dump($producto);
+echo '</pre>';
+
+// Se puede instanciar multiples productos
+$producto2 = new Producto();
+$producto2->nombre = 'Television';
+$producto2->precio = 400;
+$producto2->disponible = false;
+
+echo '<pre>';
+var_dump($producto2);
+echo '</pre>';
 
 include 'includes/footer.php';
