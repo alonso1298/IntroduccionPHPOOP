b53d084420d2f3cf336823a1bc004aee4d2182c0
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-09-22 23:23:56 -0600

    Atributos de la clase y constructores

--- 01.php
+++ 01.php
@@ -1,26 +1,31 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types = 1);
+include 'includes/header.php';
 
 // Definir o crear una clase
 class Producto {
     public $nombre;
     public $precio;
     public $disponible;
+
+    //Un constructor es una funcion que se va mandar llamar automaticamente una vez que se a creado una nueva instancia 
+    public function __construct(string $nombre, int $precio, bool $disponible) // Una funcion en una clase se le conoce como métodos y se realiza con public fuction
+    {
+        // Se asignan los valores del constructor a la instancia con this
+        $this-> nombre = $nombre;
+        $this-> precio = $precio;
+        $this-> disponible = $disponible;
+    }
 }
 
-$producto = new Producto();
-$producto->nombre = 'Tablet';
-$producto->precio = 200;
-$producto->disponible = true;
+$producto = new Producto('Tablet', 200, true);
 
 echo '<pre>';
 var_dump($producto);
 echo '</pre>';
 
 // Se puede instanciar multiples productos
-$producto2 = new Producto();
-$producto2->nombre = 'Television';
-$producto2->precio = 400;
-$producto2->disponible = false;
+$producto2 = new Producto('Montor Curvo', 400, false);
 
 echo '<pre>';
 var_dump($producto2);
