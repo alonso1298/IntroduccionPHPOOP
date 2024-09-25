e63a4c8bd5a57d0242a1d52388492161cee5f348
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-09-24 22:55:45 -0600

    Autoload en clases

--- 08.php
+++ 08.php
@@ -1,4 +1,14 @@
 <?php include 'includes/header.php';
 
+// Incluir la otras clases
+function mi_autoload($clase) { // Se crea la funcion la cual va a recibir una clase
+    require __DIR__ . '/clases/' . $clase . '.php'; // Le requerimos la ruta donde se encuentran estas clases
+}
+
+spl_autoload_register('mi_autoload'); // AutoLoad busca las clases segun se van importando 
+
+$detalles = new Detalles();
+echo '<br>';
+$clientes = new Clientes();
 
 include 'includes/footer.php';
