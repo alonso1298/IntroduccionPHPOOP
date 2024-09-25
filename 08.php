c2dd41cd1d9fe9810a046dd55c5100e2573311fd
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-09-24 23:47:00 -0600

    Autoload con composer

--- 08.php
+++ 08.php
@@ -1,19 +1,55 @@
 <?php include 'includes/header.php';
 
+require 'vendor/autoload.php';
+
 // require 'clases/Clientes.php';
 // require 'clases/Detalles.php';
 
 use App\Clientes; // Cuando se coloca de esa forma se puede eliminar el namespace
-
-// Incluir la otras clases
-function mi_autoload($clase) { // Se crea la funcion la cual va a recibir una clase
-    $partes = explode('\\', $clase); // explode toma 2 argumentos, uno es lo que vamos a buscar y el segundo en donde lo vamos a buscar
-    require __DIR__ . '/clases/' . $partes[1] . '.php'; // Le requerimos la ruta donde se encuentran estas clases
-}
-spl_autoload_register('mi_autoload'); // AutoLoad busca las clases segun se van importando 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 $detalles = new App\Detalles(); // para acceder a una clase con Namespace se pone el nombre que se dio seguido de un \ y despues el nombre de la clase
 echo '<br>';
 $clientes = new Clientes();  // No se usa el namespace ya que tenemos el use 
+echo '<br>';
+
+$key = 'example_key';
+$payload = [
+    'iss' => 'http://example.org',
+    'aud' => 'http://example.com',
+    'iat' => 1356999524,
+    'nbf' => 1357000000
+];
+
+/**
+ * IMPORTANT:
+ * You must specify supported algorithms for your application. See
+ * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
+ * for a list of spec-compliant algorithms.
+ */
+$jwt = JWT::encode($payload, $key, 'HS256');
+$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+print_r($decoded);
+
+// Pass a stdClass in as the third parameter to get the decoded header values
+$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+
+/*
+ NOTE: This will now be an object instead of an associative array. To get
+ an associative array, you will need to cast it as such:
+*/
+
+$decoded_array = (array) $decoded;
+
+/**
+ * You can add a leeway to account for when there is a clock skew times between
+ * the signing and verifying servers. It is recommended that this leeway should
+ * not be bigger than a few minutes.
+ *
+ * Source: http://self-issued.info/docs/draft-ietf-oauth-json-web-token.html#nbfDef
+ */
+JWT::$leeway = 60; // $leeway in seconds
+$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
 
 include 'includes/footer.php';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yo_al/introduccion-phpoop',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5501e42a58e434319d295115bdfab9e374d741ca',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.10.1',
+            'version' => '6.10.1.0',
+            'reference' => '500501c2ce893c824c801da135d02661199f60c5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yo_al/introduccion-phpoop' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5501e42a58e434319d295115bdfab9e374d741ca',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
