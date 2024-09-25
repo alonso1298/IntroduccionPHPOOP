12ab199ef179ea4fe82a2162cc537f175929d5ad
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-09-24 20:35:55 -0600

    Interfaces

--- 06.php
+++ 06.php
@@ -1,4 +1,24 @@
 <?php include 'includes/header.php';
 
+// Las interfaces te permiten agrupar un grupo de declaraciones de funciones pero no las van a implementar, basicamente te dice, que funciones tiene y que data retorna pero no dice como lo hace
+interface TransporteInterfaz {
+    public function getInfo() : string;
+    public function getRuedas() : int;
+}
+
+class Transporte implements TransporteInterfaz { // Para agregar la interfaz es con la palabra reservada implements
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+    }
+
+    public function getInfo() : string {
+        return 'El transporte tiene ' . $this->ruedas . ' ruedas y una capacidad de ' . $this->capacidad . ' personas';
+    }
+
+    public function getRuedas() : int {
+        return $this->ruedas;
+    } 
+
+}
 
 include 'includes/footer.php';
